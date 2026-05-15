6e096542f997af0f39d8c04f92d238090f510e0d
siam1234-Brand <md.mahabuburrahamansiam@gmail.com>
2026-05-16 00:39:17 +0600

    Add doctor and specialization management

---
+++ controller/admin/specializationHandler.php
@@ -0,0 +1,17 @@
+<?php
+include_once("../../helper/auth.php");
+include_once("../../model/HospitalModel.php");
+require_role('admin');
+$model = new HospitalModel();
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if ($_POST['action'] == 'add') {
+        if (empty($_POST['name'])) { set_msg("Specialization name required"); }
+        else { $model->addSpecialization($_POST['name'], $_POST['description']); set_msg("Specialization added"); }
+    } elseif ($_POST['action'] == 'update') {
+        $model->updateSpecialization($_POST['id'], $_POST['name'], $_POST['description']); set_msg("Specialization updated");
+    } elseif ($_POST['action'] == 'delete') {
+        $model->deleteSpecialization($_POST['id']); set_msg("Specialization deleted");
+    }
+}
+header('Location: ../../view/admin/manage_specializations.view.php');
+?>
