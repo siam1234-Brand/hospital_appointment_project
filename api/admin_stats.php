011ad9e801cd3e2b3ac4c57683315cb845baf49b
SIAM <151781585+siam1234-Brand@users.noreply.github.com>
2026-05-17 14:28:24 +0600

    Replace HospitalModel with AdminModel in admin_stats

--- api/admin_stats.php
+++ api/admin_stats.php
@@ -1,9 +1,9 @@
 <?php
 include_once("../helper/auth.php");
-include_once("../model/HospitalModel.php");
+include_once("../model/admin/AdminModel.php");
 require_role('admin');
 header('Content-Type: application/json');
-$model = new HospitalModel();
+$model = new AdminModel();
 $stats = $model->getAdminStats();
 echo json_encode($stats);
 ?>
