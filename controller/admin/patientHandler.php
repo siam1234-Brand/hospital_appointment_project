edbf929c923537d63ef6fd34f3863f86ac62e6d9
SIAM <151781585+siam1234-Brand@users.noreply.github.com>
2026-05-17 14:29:56 +0600

    Switch to AdminModel for patient handling

--- controller/admin/patientHandler.php
+++ controller/admin/patientHandler.php
@@ -1,11 +1,12 @@
 <?php
 include_once("../../helper/auth.php");
-include_once("../../model/HospitalModel.php");
+include_once("../../model/admin/AdminModel.php");
 require_role('admin');
-$model = new HospitalModel();
+$model = new AdminModel();
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $model->deactivatePatient($_POST['patient_id']);
     set_msg("Patient account deactivated");
 }
 header('Location: ../../view/admin/manage_patients.view.php');
 ?>
+
