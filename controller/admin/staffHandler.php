40dc53eac3f29350347ea5e959997e62bcfa0027
SIAM <151781585+siam1234-Brand@users.noreply.github.com>
2026-05-17 14:30:59 +0600

    Update staff handler to use AdminModel

--- controller/admin/staffHandler.php
+++ controller/admin/staffHandler.php
@@ -1,8 +1,8 @@
 <?php
 include_once("../../helper/auth.php");
-include_once("../../model/HospitalModel.php");
+include_once("../../model/admin/AdminModel.php");
 require_role('admin');
-$model = new HospitalModel();
+$model = new AdminModel();
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if ($_POST['action'] == 'add') {
         if (empty($_POST['name']) || empty($_POST['email']) || empty($_POST['password'])) {
