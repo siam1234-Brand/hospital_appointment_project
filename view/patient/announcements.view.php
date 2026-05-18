41623c3ccab8e483810a5e175287fb81203b014a
Mohammad Ali Masud <151671174+masud-ali@users.noreply.github.com>
2026-05-18 20:31:08 +0600

    Update announcements.view.php

--- view/patient/announcements.view.php
+++ view/patient/announcements.view.php
@@ -7,14 +7,23 @@ $list = $model->getAnnouncements('patient');
 ?>
 <!DOCTYPE html>
 <html>
-<head><title>Announcements</title><link rel="stylesheet" href="../../assets/style.css"></head>
+
+<head>
+    <title>Announcements</title>
+    <link rel="stylesheet" href="../../assets/style.css">
+</head>
+
 <body>
-<?php include_once("menu.php"); ?>
-<div class="container">
-<h2>Hospital Announcements</h2>
-<?php foreach($list as $a){ ?>
-<div class="card"><h3><?php echo $a['title']; ?></h3><p><?php echo $a['body']; ?></p><small><?php echo $a['published_at']; ?> by <?php echo $a['author']; ?></small></div>
-<?php } ?>
-</div>
+    <?php include_once("menu.php"); ?>
+    <div class="container">
+        <h2>Hospital Announcements</h2>
+        <?php foreach ($list as $a) { ?>
+            <div class="card">
+                <h3><?php echo $a['title']; ?></h3>
+                <p><?php echo $a['body']; ?></p><small><?php echo $a['published_at']; ?> by <?php echo $a['author']; ?></small>
+            </div>
+        <?php } ?>
+    </div>
 </body>
+
 </html>
