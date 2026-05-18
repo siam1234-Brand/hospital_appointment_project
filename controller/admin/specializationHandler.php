59b180ca0e493bb5a2426bc36788be0be89494da
SIAM <151781585+siam1234-Brand@users.noreply.github.com>
2026-05-19 00:24:34 +0600

    Refactor specializationHandler for better readability

--- controller/admin/specializationHandler.php
+++ controller/admin/specializationHandler.php
@@ -5,13 +5,18 @@ require_role('admin');
 $model = new AdminModel();
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if ($_POST['action'] == 'add') {
-        if (empty($_POST['name'])) { set_msg("Specialization name required"); }
-        else { $model->addSpecialization($_POST['name'], $_POST['description']); set_msg("Specialization added"); }
+        if (empty($_POST['name'])) {
+            set_msg("Specialization name required");
+        } else {
+            $model->addSpecialization($_POST['name'], $_POST['description']);
+            set_msg("Specialization added");
+        }
     } elseif ($_POST['action'] == 'update') {
-        $model->updateSpecialization($_POST['id'], $_POST['name'], $_POST['description']); set_msg("Specialization updated");
+        $model->updateSpecialization($_POST['id'], $_POST['name'], $_POST['description']);
+        set_msg("Specialization updated");
     } elseif ($_POST['action'] == 'delete') {
-        $model->deleteSpecialization($_POST['id']); set_msg("Specialization deleted");
+        $model->deleteSpecialization($_POST['id']);
+        set_msg("Specialization deleted");
     }
 }
 header('Location: ../../view/admin/manage_specializations.view.php');
-?>
