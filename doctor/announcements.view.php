6fd04063fd148b7059267375a1bd65569a36e4fc
maya0823 <23-54378-3@student.aiub.edu>
2026-05-19 05:56:30 +0600

    Refactor announcements view for better structure

--- doctor/announcements.view.php
+++ doctor/announcements.view.php
@@ -7,12 +7,21 @@ $list = $model->getAnnouncements('doctor');
 ?>
 <!DOCTYPE html>
 <html>
-<head><title>Announcements</title><link rel="stylesheet" href="../../assets/style.css"></head>
+<head>
+    <title>Announcements</title>
+    <link rel="stylesheet" href="../../assets/style.css">
+</head>
 <body>
-<?php include_once("menu.php"); ?>
-<div class="container">
-<h2>Doctor Announcements</h2>
-<?php foreach($list as $a){ ?><div class="card"><h3><?php echo $a['title']; ?></h3><p><?php echo $a['body']; ?></p><small><?php echo $a['published_at']; ?></small></div><?php } ?>
-</div>
+    <?php include_once("menu.php"); ?>
+    <div class="container">
+        <h2>Doctor Announcements</h2>
+        <?php foreach($list as $a){ ?>
+        <div class="card">
+            <h3><?php echo $a['title']; ?></h3>
+            <p><?php echo $a['body']; ?></p>
+            <small><?php echo $a['published_at']; ?></small>
+        </div>
+        <?php } ?>
+    </div>
 </body>
 </html>
