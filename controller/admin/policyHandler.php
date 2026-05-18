6fd45782e3529b35a73f2949647861a6a80e21ce
SIAM <151781585+siam1234-Brand@users.noreply.github.com>
2026-05-19 00:24:09 +0600

    Refactor policy handling and update redirection

--- controller/admin/policyHandler.php
+++ controller/admin/policyHandler.php
@@ -4,19 +4,11 @@ include_once("../../model/admin/AdminModel.php");
 require_role('admin');
 $model = new AdminModel();
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if ($_POST['action'] == 'add') {
-        if (empty($_POST['name'])) {
-            set_msg("Specialization name required");
-        } else {
-            $model->addSpecialization($_POST['name'], $_POST['description']);
-            set_msg("Specialization added");
-        }
-    } elseif ($_POST['action'] == 'update') {
-        $model->updateSpecialization($_POST['id'], $_POST['name'], $_POST['description']);
-        set_msg("Specialization updated");
-    } elseif ($_POST['action'] == 'delete') {
-        $model->deleteSpecialization($_POST['id']);
-        set_msg("Specialization deleted");
+    if ($_POST['setting_value'] === '') {
+        set_msg("Policy value cannot be empty");
+    } else {
+        $model->updateSetting($_POST['setting_name'], $_POST['setting_value']);
+        set_msg("Policy updated");
     }
 }
-header('Location: ../../view/admin/manage_specializations.view.php');
+header('Location: ../../view/admin/policies.view.php');
