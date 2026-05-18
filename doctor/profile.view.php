b19d7683f147dcedf9c112ec52d0cd9f20308322
maya0823 <23-54378-3@student.aiub.edu>
2026-05-19 05:28:17 +0600

    Enhance profile form layout and labels

--- doctor/profile.view.php
+++ doctor/profile.view.php
@@ -18,13 +18,13 @@ $specs = $model->getSpecializations();
 <?php if ($d['photo_path'] != "") { ?><img class="small-img" src="../../<?php echo $d['photo_path']; ?>"><?php } ?>
 <form method="post" action="../../controller/doctor/profileHandler.php" enctype="multipart/form-data">
     <textarea name="bio" placeholder="Bio"><?php echo $d['bio']; ?></textarea><br>
-    <select name="specialization_id">
+    Specialization: <select name="specialization_id">
         <?php foreach($specs as $s){ ?><option value="<?php echo $s['id']; ?>" <?php selected($d['specialization_id'], $s['id']); ?>><?php echo $s['name']; ?></option><?php } ?>
     </select><br>
-    <input type="number" name="consultation_fee" value="<?php echo $d['consultation_fee']; ?>" placeholder="Fee"><br>
-    <input type="text" name="license_number" value="<?php echo $d['license_number']; ?>" placeholder="License Number"><br>
-    <input type="number" name="experience_years" value="<?php echo $d['experience_years']; ?>" placeholder="Experience Years"><br>
-    Profile Photo:<br><input type="file" name="photo"><br>
+    Consultation Fee: <input type="number" name="consultation_fee" value="<?php echo $d['consultation_fee']; ?>" placeholder="Fee"><br>
+    License Number: <input type="text" name="license_number" value="<?php echo $d['license_number']; ?>" placeholder="License Number"><br>
+    Experience Years: <input type="number" name="experience_years" value="<?php echo $d['experience_years']; ?>" placeholder="Experience Years"><br>
+    Profile Photo: <br><input type="file" name="photo"><br>
     <input type="submit" value="Update Profile">
 </form>
 </div>
